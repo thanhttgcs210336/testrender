3b055c238e89df17d07b6e38dbf9fa37ef4e3cf2
thanh tran <thanhttgcs210336@fpt.edu.vn>
2023-05-27 20:15:21 +0700

    hello

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+app.get('/', (req, res) => {
+res.send('Hello, Thanh')
+})
+app.listen(3000, () => {
+console.log('Server started on port 3000')
+})
